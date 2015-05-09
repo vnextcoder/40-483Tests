006b2c7be06487f2ac98fe9c6691b4fb7169a632
avinb <avinb@users.noreply.github.com>
2015-05-09 14:22:42 +0100

    commiting french
    commiting french

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World from Git HUB master");
-
+Console.WriteLine("should have french as well ");
             Console.WriteLine("I am german now");
         }
     }
