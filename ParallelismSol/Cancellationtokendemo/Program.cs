e6828fb87d98ecad510c5c137c093fde923d482e
bavi <bavi014@gmail.com>
2015-06-27 19:40:28 +0100

    added all files

--- ParallelismSol/Cancellationtokendemo/Program.cs
+++ ParallelismSol/Cancellationtokendemo/Program.cs
@@ -11,12 +11,17 @@ namespace Cancellationtokendemo
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
-        static void Main()
+        //[STAThread]
+        //static void Main()
+        //{
+        //    Application.EnableVisualStyles();
+        //    Application.SetCompatibleTextRenderingDefault(false);
+        //    Application.Run(new Form1());
+        //}
+   
+        static void Main2()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            PLINQCancellation.GetPrimeNumbers();
         }
     }
 }
