ddf0b217d981225fa47afa270a5e34774f9efdc6
training135 <training135@BirlasoftCOE.com>
2018-06-12 13:06:36 +0530

    mysecureapp

---
+++ mysecureapp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using mysecureapp.Data;
+// [assembly: HostingStartup(typeof(mysecureapp.Areas.Identity.IdentityHostingStartup))]
+// namespace mysecureapp.Areas.Identity
+// {
+//     public class IdentityHostingStartup : IHostingStartup
+//     {
+//         public void Configure(IWebHostBuilder builder)
+//         {
+//             builder.ConfigureServices((context, services) => {
+//                 services.AddDbContext<ApplicationDbContext>(options =>
+//                     options.UseSqlServer(
+//                         context.Configuration.GetConnectionString("ApplicationDbContextConnection")));
+
+//                 services.AddDefaultIdentity<IdentityUser>()
+//                     .AddEntityFrameworkStores<ApplicationDbContext>();
+//             });
+//         }
+//     }
+// }
